abc20f7af074490dbe8bfc98d4657d6f1afc7c2c
Mohit <mohitsharma8864@gmail.com>
2022-09-08 19:07:30 +0530

    styled components

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    flex:1;
+`;
+export default function Menu(){
+  return (
+    <Container>Menu</Container>
+  )
+}
