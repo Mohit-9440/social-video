890c2c8775d704b2e30379864e973c0abded2807
Mohit <mohitsharma8864@gmail.com>
2022-09-12 00:53:39 +0530

    Router DOM

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``;
+
+export default function Home() {
+  return (
+    <Container>Home</Container>
+  )
+}
